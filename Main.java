e2101a78193f21af6827153b68a4d6fedf0c6c87
roldebijvank <r.olde.bijvank@student.tue.nl>
2023-10-10 17:06:42 +0200

    Second commit, new branch

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("First Commit!");
+        System.out.println("Second Commit in new branch!");
     }
 }
