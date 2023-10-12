39850c65fa17b8bab6edd8e561f2c439c9743f91
martastepien <martunia.stepien@gmail.com>
2023-10-12 17:09:21 +0200

    Window created

---
+++ DiamondHeist.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+public class DiamondHeist {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+
+
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1200, 700);
+        frame.setTitle("Diamond Heist");
+        frame.setVisible(true);
+    }
+}
