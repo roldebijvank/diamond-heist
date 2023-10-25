3628c947624a9f68fd0a6a6bba04872fe173e4e3
roldebijvank <r.olde.bijvank@student.tue.nl>
2023-10-25 23:57:37 +0200

    fixed errors in push and fixed checkstyle warnings

--- Diamond.java
+++ Diamond.java
@@ -1,9 +1,10 @@
 import java.net.URL;
 
+/**
+ * Creates Diamond object which extends CollectableItem.
+ */
 public class Diamond extends CollectableItem {
     public Diamond(URL imageUrl) {
         super(imageUrl, 1000, 100, 60, 60); 
     }
-
-    // add code that allows to exit only after the diamond is collected. @TODO: create exit door
 }
--- Key.java
+++ Key.java
@@ -1,10 +1,10 @@
 import java.net.URL;
 
+/**
+ * Creates Key object which extends CollectableItem.
+ */
 public class Key extends CollectableItem {
     public Key(URL imageUrl) {
         super(imageUrl, 10, 170, 50, 50); 
     }
-
-    // add code that allows to go through hatch only after the key is collected. 
-    //@TODO: create hatch door
 }
