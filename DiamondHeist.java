dc4b6881920f2d98efe1bef3ff03df549fc96d84
roldebijvank <r.olde.bijvank@student.tue.nl>
2023-10-29 16:29:53 +0100

    test for windows performance

--- DiamondHeist.java
+++ DiamondHeist.java
@@ -229,6 +229,7 @@ public class DiamondHeist {
         if (System.getProperty("os.name").contains("Windows")) {
             System.setProperty("sun.java2d.noddraw", Boolean.TRUE.toString());
         }
+        System.out.println("OS: " + System.getProperty("os.name"));
         diamondHeist = new DiamondHeist();
         frame = new JFrame("Diamond Heist");
         frame.setLayout(null);
